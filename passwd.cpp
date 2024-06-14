be4dfdb618c986c768ef5a2e3a35ff717e35d4df
puppykiwi <johnraymwendwa04@gmail.com>
2024-06-14 12:51:16 +0300

    bump

--- passwd.cpp
+++ passwd.cpp
@@ -1,14 +1,5 @@
-#include <iostream>
-#include <typeinfo>
-#include <limits>
 #include "passwd.h"
 
-template <typename T>
-void var_type (T var) {
-    std::cout << typeid(var).name() << std::endl;
-}
-
-<<<<<<< HEAD
 
 /*
 int get_length(bool running){
@@ -18,25 +9,12 @@ int get_length(bool running){
     while (true){
         std::cout << "What is the desired length of your program: ";
         std::cin >> size;
-=======
-int main(){
-     
-    int length = get_length();
-}
-
-int get_length(){
-
-    int size;
-    std::cout << "What is the desired length of your program: ";
-    std::cin >> size;
->>>>>>> parent of 9ac6d86 (uhhu)
 
     // const char * type_length = typeid(size).name();
     // std::cout << type_length << std::endl ;
     // var_type(size);
     
     
-<<<<<<< HEAD
         if (size > 0){
             // std::cout << "test"; //debug
             return size;
@@ -45,16 +23,6 @@ int get_length(){
             std::cout << "Your value should be an integer and larger than 0";
             
         }
-=======
-
-    if (size > 0){
-        // std::cout << "test"; //debug
-        return size;
-    }
-    else {
-        std::cout << "Your value should be an integer and larger than 0";
-        return get_length();
->>>>>>> parent of 9ac6d86 (uhhu)
     }
     
     return 0;
--- passwd.h
+++ passwd.h
@@ -3,3 +3,9 @@
 #include <limits>
 
 int get_length();
+
+template <typename T>
+void var_type (T var) {
+    std::cout << typeid(var).name() << std::endl;
+}
+
