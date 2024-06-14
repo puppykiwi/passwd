ac9bc4554f8803799831fc440592ca83a8c118bc
puppykiwi <johnraymwendwa04@gmail.com>
2024-06-14 17:07:41 +0300

    string

--- passwd.h
+++ passwd.h
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <typeinfo>
 #include <limits>
+#include <random>
+#include <string>
 
 int get_length();
+
+std::string gen_passwd(int length);
 
 template <typename T>
 void var_type (T var) {
